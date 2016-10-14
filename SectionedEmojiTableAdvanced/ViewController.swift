3dee54e673472726a368787829ac5dde8c73b100
YuryG <yury.gitman@gmail.com>
2016-10-14 13:21:49 -0400

    More Advanced Table

--- SectionedEmojiTableAdvanced/ViewController.swift
+++ SectionedEmojiTableAdvanced/ViewController.swift
@@ -8,7 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+let flowersArray = ["🌻","🌺","🌹","🌷","🌸"]
+let insectArray = ["🐌","🐞","🐝","🐛","🕷"]
+let birdsArray = ["🐦","🐧","🐔","🐤","🐣"]
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate  {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +23,74 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+            return 3
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let mySection = indexPath.section
+        
+        if mySection == 0{
+         
+            let myCell = tableView.dequeueReusableCell(withIdentifier: "blueCell")
+            return myCell!
+        }
+        else if mySection == 1{
+            let myCell = tableView.dequeueReusableCell(withIdentifier: "greenCell")
+            return myCell!
+            
+        }  else if mySection == 2{
+            let myCell = tableView.dequeueReusableCell(withIdentifier: "yellowCell")
+            return myCell!
+        
+        } else {
+            
+            let myCell = tableView.dequeueReusableCell(withIdentifier: "errorCell")
+            return myCell!
+            
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return CGFloat(70)
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        
+        let mySection = section
+        
+        if mySection == 0 {
+            return "Flower"
+        } else if mySection == 1{
+            return "Insects"
+        } else if mySection == 2 {
+            return "Birds"
+        } else {
+        return "Header"
+        }
+    }
 
+    func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        
+        let title = view as! UITableViewHeaderFooterView
+        title.textLabel?.font = UIFont(name: "Futura", size: 60)
+        
+    }
+    
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return CGFloat(10)
+    }
+    
+    
 
 }
+
+
 
